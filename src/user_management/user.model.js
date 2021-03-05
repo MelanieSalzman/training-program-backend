d07f4a04ef9d9a166c50e7357a720d300f64afc1
Melanie Salzman <salzmanmelanie@gmail.com>
2021-03-05 10:02:49 -0300

    Implement User Model

--- src/user_management/user.model.js
+++ src/user_management/user.model.js
@@ -1,11 +1,39 @@
+import bcrypt from 'bcrypt'
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  })
+  },
+    {
+      instanceMethods: {
+        hashPassword = (password) => {
+          return bcrypt.hash(password, bcrypt.genSaltSync(10));
+        },
+        validatePassword = (password) => {
+          return bcrypt.compare(password, this.password);
+        }
+      }
+    })
 
   return User
 }
